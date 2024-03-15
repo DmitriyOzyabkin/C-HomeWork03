e38f1fe94b92780c072d37aad05fda5ea3730fad
Dmitriy Ozyabkin <ozyabkin.d@gmail.com>
2024-03-15 22:14:05 +0300

    Task04 completed

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -6,4 +6,33 @@
 // должен быть равен количеству цифр.
 
 
+using System.Globalization;
 
+Console.Write("Enter number: ");
+string strNumber = Console.ReadLine()!;
+int index = strNumber.Length;
+int initNumber = Convert.ToInt32(strNumber);
+int[] numArray = new int[index];
+
+for (int rang = index; rang > 0; rang--)  // Убывающий цикл для поиска числа от старшего разряда к младнему 
+{
+    int number = initNumber;              // Переменая для поиска числа в разряде
+    for (int i = 1; i <= rang; i++)
+    {
+        int digit = number % 10;          // запись в digit крайнего левого числа
+        if (i == rang)                    // Если верно, достигли старшего разряда
+        {
+            numArray[index - rang] = digit;         // Пишем в массив полученное число
+        }
+        number /= 10;                                // Отбрасываем последний разряд
+    }
+}
+
+
+
+Console.Write("Numbers in array: [ ");
+foreach (int item in numArray)
+{
+    Console.Write($"{item} ");
+}
+Console.Write("]");
